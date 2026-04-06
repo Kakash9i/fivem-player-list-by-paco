6b793184ed6411220f3706775a89e4a116f851b7
Kakash9i <116752380+Kakash9i@users.noreply.github.com>
2026-04-06 04:19:13 +0200

    Create worker.js

---
+++ fivem-player-list-main/worker.js
@@ -0,0 +1,16 @@
+export default {
+  async fetch(request) {
+    const url = new URL(request.url);
+    const target = 'https://servers-frontend.fivem.net/api' + url.pathname + url.search;
+
+    const response = await fetch(target);
+    const data = await response.json();
+
+    return new Response(JSON.stringify(data), {
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+      },
+    });
+  },
+};
